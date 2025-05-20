e652d53aaffb31c858837c2aa57d10245a688981
Patrick Lu <patrick3lu@gmail.com>
2025-05-20 13:25:34 +0200

    Create base component. Remove placeholders

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./global-styles.css";
+import QRGenerator from "./components/QRGenerator/";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QRGenerator />
+  </StrictMode>
+);
